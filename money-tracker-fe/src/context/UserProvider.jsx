ab32d8d40962f7e51a74feb1ce40a368be0d025d
adam.metal <adam.metal@codecool.com>
2023-03-21 00:20:40 +0100

    add very basic user handling

--- money-tracker-fe/src/context/UserProvider.jsx
+++ money-tracker-fe/src/context/UserProvider.jsx
@@ -1,3 +1,53 @@
-import { createContext } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 const UserContext = createContext(null);
+
+const getToken = () => window.localStorage.getItem("token");
+const setToken = (token) => window.localStorage.setItem("token", token);
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
+  const getMe = (token) => {
+    fetch("/api/auth/me", {
+      headers: {
+        authorization: `bearer ${token}`,
+      },
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        setUser(user);
+      });
+  };
+
+  useEffect(() => {
+    const token = getToken();
+
+    if (!token) {
+      return;
+    }
+
+    getMe(token);
+  }, []);
+
+  const login = (creds) => {
+    fetch("/api/auth/sign-in", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(creds),
+    }).then((token) => {
+      setToken(token);
+      getMe(token);
+    });
+  };
+
+  return (
+    <UserContext.Provider value={{ user, login }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
