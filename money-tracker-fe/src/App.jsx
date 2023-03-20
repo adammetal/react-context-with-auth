15e1dc9fa63bc154d5158023da2f75b4098b7927
adam.metal <adam.metal@codecool.com>
2023-03-21 00:05:32 +0100

    add useFetch utility hook

--- money-tracker-fe/src/App.jsx
+++ money-tracker-fe/src/App.jsx
@@ -2,21 +2,17 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import { useThemeToggle } from "./context/ThemeProvider";
+import useFetch from "./hooks/useFetch";
 
 function App() {
-  const [ping, setPing] = useState(null);
+  const { data: ping, fetcher } = useFetch('/api/ping');
   const toggleTheme = useThemeToggle();
-
-  useEffect(() => {
-    fetch("/api/ping")
-      .then((r) => r.json())
-      .then((r) => setPing(r));
-  }, []);
 
   return (
     <div className="App">
       <h1>Track your spendings</h1>
       <Button text="Toggle Theme" onClick={toggleTheme} />
+      <Button text="Refetch stats" onClick={fetcher} />
       <pre>{JSON.stringify(ping, null, 2)}</pre>
     </div>
   );
--- money-tracker-fe/src/hooks/useFetch.js
+++ money-tracker-fe/src/hooks/useFetch.js
@@ -1,1 +1,39 @@
-// TODO
+import { useCallback, useEffect, useState } from "react";
+
+const useFetch = (url, instant = true) => {
+  const [loading, setLoading] = useState(instant);
+  const [fetching, setFetching] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetcher = useCallback(
+    (cfg = {}) => {
+      setFetching(true);
+
+      fetch(url, cfg)
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(err);
+        })
+        .finally(() => {
+          setFetching(false);
+          setLoading(false);
+        });
+    },
+    [url]
+  );
+
+  useEffect(() => {
+    if (instant === true) {
+      fetcher();
+    }
+  }, [instant]);
+
+  return { loading, fetching, data, error, fetcher };
+};
+
+export default useFetch;
